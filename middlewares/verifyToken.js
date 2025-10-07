168c69838fc7e615a91b63df4ea1faddd201bec1
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-10-07 19:32:05 +0300

    add auth routes

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,17 +1,20 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
   try {
-    const token = req.cookies.jwt;
-    if (!token) {
-      return res.status(401).json({ error: "Not authorized" });
-    }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.userId = decoded.userId;
-
+    req.userRole = decoded.role;
+    req.isVerified = decoded.isVerified;
+    if (!req.isVerified) {
+      return res.status(403).json({ error: "Email not verified" });
+    }
     next();
   } catch (error) {
-    res.status(401).json({ error: "Not authorized, invalid token" });
+    return res.status(401).json({ error: "Invalid token" });
   }
 };
