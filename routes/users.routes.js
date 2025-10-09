9dd5e98241d1bc7f26429afc1586cb12ce8069e8
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-10-09 10:46:20 +0300

    updates

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getAllUsers, getUserById } from "../controllers/users.controller.js";
 import { verifyAdmin } from "../middlewares/verifyAdmin.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", verifyAdmin, getAllUsers);
-router.get("/:id", getUserById);
+router.get("/", verifyToken, verifyAdmin, getAllUsers);
+router.get("/:id", verifyToken, verifyAdmin, getUserById);
 
 export default router;
