d9f7d90dca6ca799dafb56ff0a1a33535bd00d6f
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-10-09 10:48:08 +0300

    updates

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -1,5 +1,5 @@
 export const verifyAdmin = (req, res, next) => {
-  if (req.userRole !== "admin") {
+  if (req.userRole !== "admin" && req.userRole !== "manager") {
     return res.status(403).json({ error: "Access denied, admin only" });
   }
   next();
