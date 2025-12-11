c465b8d1dd2061418207494d6f0cd84065d45ce7
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-12-11 13:09:00 +0200

    updates

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,7 @@ import {
   getUsers,
   getUser,
   getAdmins,
+  deleteUser,
 } from "../controllers/users.controller.js";
 import { authenticate } from "../middleware/auth.js";
 import { authorizeAdmin } from "../middleware/auth.js";
@@ -12,5 +13,6 @@ const router = express.Router();
 router.get("/", authenticate, authorizeAdmin, getUsers);
 router.get("/:id", authenticate, authorizeAdmin, getUser);
 router.get("/admins", authenticate, authorizeAdmin, getAdmins);
+router.delete("/:id", authenticate, authorizeAdmin, deleteUser);
 
 export default router;
